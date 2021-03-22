31514424c71c6b76a833b8c20f66a51ffc07fedd
Mike Sheldon <mike@mikeasoft.com>
2021-03-22 17:13:30 +0000

    Make abstract driver variable names more consistent with USB spec and fix missing self parameters

--- pyandon/drivers/andon_driver.py
+++ pyandon/drivers/andon_driver.py
@@ -3,25 +3,25 @@ from abc import ABC, abstractmethod
 class AndonDriver(ABC):
 
     @abstractmethod
-    def usb_id_supported(manufacturer_id, device_id):
-        """
-        Returns true if this driver supports the provider manufacturer and device IDs.
-
-        Parameters:
-            manufacturer_id - string, USB manufacturer ID
-            device_id - string, USB device ID
-
-        Returns:
-            Boolean - Whether this device is supported by this driver.
-        """
-        pass
-
-    @abstractmethod
     def name(self):
         pass
 
     @abstractmethod
-    def set_light(color, pattern)
+    def usb_id_supported(self, vendor_id, product_id):
+        """
+        Returns true if this driver supports the provider vendor and product IDs.
+
+        Parameters:
+            vendor_id - string, USB vendor ID
+            product_id - string, USB product ID
+
+        Returns:
+            Boolean - Whether this product is supported by this driver.
+        """
+        pass
+
+    @abstractmethod
+    def set_light(self, color, pattern):
         """
         Sets the specified light to display the given pattern.
 
