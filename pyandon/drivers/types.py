1b713fbb0581c2cd98de6de7a43b63e958eafc66
Mike Sheldon <mike@mikeasoft.com>
2021-03-23 12:34:10 +0000

    Add enums

---
+++ pyandon/drivers/types.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class DriverType(Enum):
+    USB = 1
+    Network = 2
+
+class LightColor(Enum):
+    RED = 1
+    YELLOW = 2
+    GREEN = 3
+    BLUE = 4
+    CLEAR = 5
+
+class LightPattern(Enum):
+    OFF = 0
+    STATIC = 1
+    QUICK_FLASH = 2
+    SLOW_FLASH = 3
+    FLASH_FLASH_PAUSE = 4
+    FLASH_PAUSE = 5
+
