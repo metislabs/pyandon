bdd364c8f250c1c9de7d6e4d79639b2b59f597f0
Mike Sheldon <mike@mikeasoft.com>
2021-03-24 12:39:05 +0000

    Fix dynamic driver loading

--- pyandon/drivers/__init__.py
+++ pyandon/drivers/__init__.py
@@ -1,10 +1,13 @@
 import pkgutil
+import os.path
 
 def load_drivers():
     drivers = []
-    for submodule in pkgutil.walk_packages(["drivers"]):
-        if submodule.name != "driver":
-            l = pkgutil.get_loader("drivers." + submodule.name)
+    loader = pkgutil.get_loader("pyandon.drivers")
+    for submodule in pkgutil.walk_packages([os.path.dirname(loader.path)]):
+        print(submodule)
+        if submodule.name != "andon_driver" and submodule.name != "andon_usb_driver":
+            l = pkgutil.get_loader("pyandon.drivers." + submodule.name)
             m = l.load_module()
             drivers.append(m.create_driver())
     return drivers
