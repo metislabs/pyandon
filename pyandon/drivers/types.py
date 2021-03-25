dfd146f95fa6d76284f808fe0d74a338e94c3eb1
Mike Sheldon <mike@mikeasoft.com>
2021-03-25 10:58:13 +0000

    Add notes for buzzers

--- pyandon/drivers/types.py
+++ pyandon/drivers/types.py
@@ -26,3 +26,19 @@ class BuzzerPattern(IntEnum):
     PATTERN_2 = 3
     PATTERN_3 = 4
     PATTERN_4 = 5
+
+class BuzzerNotes(IntEnum):
+    OFF = 0
+    A6 = 1760
+    BFLAT6 = 1865
+    B6 = 1976
+    C7 = 2093
+    DFLAT7 = 2217
+    D7 = 2349
+    EFLAT7 = 2489
+    E7 = 2637
+    F7 = 2794
+    GFLAT7 = 2960
+    G7 = 3136
+    AFLAT7 = 3322
+    A7 = 3520
