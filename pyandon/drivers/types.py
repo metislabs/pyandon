0d2208e0862916ad757fa8478161b74bf73632d8
Mike Sheldon <mike@mikeasoft.com>
2021-03-25 14:14:38 +0000

    Add license message to all files

--- pyandon/drivers/types.py
+++ pyandon/drivers/types.py
@@ -1,8 +1,34 @@
+"""
+PyAndon - Python module for interacting with a range of different andon lights
+
+Copyright 2021, Metis Labs Ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from enum import Enum, IntEnum
+
 
 class DriverType(Enum):
     USB = 1
     Network = 2
+
 
 class LightColor(Enum):
     RED = 1
@@ -10,6 +36,7 @@ class LightColor(Enum):
     GREEN = 3
     BLUE = 4
     CLEAR = 5
+
 
 class LightPattern(IntEnum):
     OFF = 0
@@ -19,6 +46,7 @@ class LightPattern(IntEnum):
     FLASH_FLASH_PAUSE = 4
     FLASH_PAUSE = 5
 
+
 class BuzzerPattern(IntEnum):
     OFF = 0
     ON = 1
@@ -26,6 +54,7 @@ class BuzzerPattern(IntEnum):
     PATTERN_2 = 3
     PATTERN_3 = 4
     PATTERN_4 = 5
+
 
 class BuzzerNotes(IntEnum):
     OFF = 0
