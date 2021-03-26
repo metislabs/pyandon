fcb7b79c1eaef4001553f0fb61b0b76ae10ef0eb
Mike Sheldon <mike@mikeasoft.com>
2021-03-26 12:28:46 +0000

    Fix installation of drivers

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['pyandon'],
+    packages=['pyandon', 'pyandon.drivers'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
